2495f0ff4bbfadb8b239ba8ce72be5f147515ba2
Lukáš Chábek <lukaschabek@gmail.com>
2019-02-04 01:50:39 +0100

    Added functionality to the bot.
    Added creating a direct message channel and sending welcome message.

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ var fs = require("fs");
 const client = new Discord.Client();
 const token = fs.readFileSync("token.txt").toString();
 
+client.on("guildMemberAdd", member => {
+	member.createDM()
+	.then(dmchanel => {
+		dmchanel.send(`Welcome to the server ${member.displayName}`);
+	})
+	.catch(reason => {
+		console.error(`Couldn't send a welcome message to the user because ${reason}.`);
+	});
+});
+
 client.login(token);
